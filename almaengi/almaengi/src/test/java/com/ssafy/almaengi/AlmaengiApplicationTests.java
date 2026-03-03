f5208e345b938a5279154b45f9c2b80806c0baf8
정혜원 <dnjs3023@gmail.com>
2026-03-03 11:00:35 +0900

    init: BE project init

---
+++ almaengi/almaengi/src/test/java/com/ssafy/almaengi/AlmaengiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.almaengi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmaengiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
