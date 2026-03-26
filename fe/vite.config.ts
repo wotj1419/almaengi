814690e72d1398680a0d7bfa56a237538831d316
함지수 <hjsu0825@naver.com>
2026-03-26 17:57:17 +0900

    [FE] fix: 빌드 시 파일 크기 경고 해결

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -18,6 +18,18 @@ export default defineConfig({
       },
     },
   },
+  // 빌드 시 파일 크기 경고 해결 및 성능 최적화를 위한 청크 분할 설정 추가
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // 외부 라이브러리들을 'vendor'라는 별도 파일로 분리
+          }
+        },
+      },
+    },
+  },
   plugins: [
     react(),
     tailwindcss(),
